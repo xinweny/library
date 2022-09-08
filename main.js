a03aa122645d6dbc8b3ac35a85a6f17628e01295
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-08 14:28:55 -0400

    Add function to display books on page

--- main.js
+++ main.js
@@ -15,7 +15,36 @@
     new Book("Raccoons Are The Brightest People", 'Sterling North', 1966, 192, false));
 
 
-  function addBookToLibrary() {
-
+  function addBookToLibrary(book) {
+    library.push(book);
   }
+
+  function createBookCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.appendChild(document.createElement('h2')
+      .appendChild(document.createTextNode(book.title)));
+    card.appendChild(document.createElement('p')
+      .appendChild(document.createTextNode(book.author)));
+    card.appendChild(document.createElement('p')
+      .appendChild(document.createTextNode(book.year)));
+    card.appendChild(document.createElement('p')
+      .appendChild(document.createTextNode(book.pages)));
+      card.appendChild(document.createElement('button')
+      .appendChild(document.createTextNode(book.isRead)));
+
+    return card;
+  }
+
+  function displayBooks() {
+    const libraryContainer = document.querySelector('.content');
+
+    for (let book of library) {
+      let card = createBookCard(book);
+      libraryContainer.appendChild(card);
+    }
+  }
+
+  displayBooks();
  })();
