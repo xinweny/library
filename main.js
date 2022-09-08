0b6bfdf7cc989895af9bfb4144416b2fbd016156
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-08 14:00:03 -0400

    Add default books

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 (function() {
   let library = [];
 
-  function Book(title, author, pages, isRead=false) {
+  function Book(title, author, year, pages, isRead=false) {
     this.title = title;
     this.author = author;
+    this.year = year;
     this.pages = pages;
     this.isRead = isRead;
   }
+
+  library.push(
+    new Book("Ducks: And how to Make Them Pay", 'William Cook', 1894, 188, true),
+    new Book("How to Teach Quantum Physics to your Dog", 'Chad Orzel', 2010, 256, true),
+    new Book("Raccoons Are The Brightest People", 'Sterling North', 1966, 192, false));
+
 
   function addBookToLibrary() {
 
