853609a5653b4836b45a2cfd187fd0f0a4d8b9c8
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-08 13:44:18 -0400

    Add Book constructor

---
+++ main.js
@@ -0,0 +1,14 @@
+(function() {
+  let library = [];
+
+  function Book(title, author, pages, isRead=false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+
+  function addBookToLibrary() {
+
+  }
+ })();
